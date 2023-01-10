ff5a206a2e7ecafb168f38ee2d8ac70172709b6b
Nowsath <nowsath598@gmail.com>
2023-01-10 15:55:05 +0530

    firstIssues Cleared

---
+++ Project1Product/Project1Product/Product.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1Product
+{
+    class Product
+    {
+        int _id;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        string _category;
+
+        public string Category
+        {
+            get { return _category; }
+            set { _category = value; }
+        }
+
+        double _unitprice;
+
+        public double Unitprice
+        {
+            get { return _unitprice; }
+            set { _unitprice = value; }
+        }
+
+        double _taxprecent;
+
+        public double Taxprecent
+        {
+            get { return _taxprecent; }
+            set { _taxprecent = value; }
+        }
+
+        string _Status;
+
+        public string Status
+        {
+            get { return _Status; }
+            set { _Status = value; }
+        }
+
+    }
+}
--- Project1Product/Project1Product/Program.cs
+++ Project1Product/Project1Product/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Project1Product
 {
@@ -10,6 +11,54 @@ namespace Project1Product
     {
         static void Main(string[] args)
         {
+            string inputFilePath = "C:\\Users\\Nowsath\\eclipse-workspace\\ProjectFirst\\src\\resource\\Product.txt";
+
+            //to Create folder
+            //string path = "C:\\MySampleInput";
+            //CreateDirectory(path);
+
+            Console.WriteLine("Read all lines as an array and access array elements");
+            string[] myFile = File.ReadAllLines(inputFilePath);
+            string hir = myFile[0];
+            string[] array_1 = null;
+
+            char[] split_character = { '|' };
+
+            array_1 = hir.Split(split_character);
+
+            Console.WriteLine(array_1[2]);
+
+            Product det = new Product();
+            det.Id = Convert.ToInt32(array_1[0]);
+            det.Name = array_1[1];
+            det.Category = array_1[2];
+            det.Unitprice = Convert.ToDouble(array_1[3]);
+            det.Taxprecent = Convert.ToDouble(array_1[4]);
+            det.Status = array_1[5];
+            // Create dictonary 
+            Dictionary<int, Product> md = new Dictionary<int, Product>();
+            md.Add(104, det);
+
+            foreach (KeyValuePair<int,Product>  x in md)
+            {
+                //Console.WriteLine(x.Key);
+                Product p1 = x.Value;
+                Console.WriteLine(p1.Id);
+                Console.WriteLine(p1.Name);
+                Console.WriteLine(p1.Category);
+                Console.WriteLine(p1.Unitprice);
+                Console.WriteLine(p1.Taxprecent);
+                Console.WriteLine(p1.Status);
+            }
+            
+
+
+
         }
+        private static void CreateDirectory(string path)
+        {
+            Directory.CreateDirectory(path);
+        }
+        
     }
 }
